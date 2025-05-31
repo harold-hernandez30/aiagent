e7ccaa3b80aba1775779c9f8d4766d7c1d27ebec
Harold Hernandez <harold.hernandez@visma.com>
2025-05-31 18:59:58 +0200

    Add verbose flag

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ if len(argv) == 1:
     print("error: please provide prompt")
     exit(1)
 
-user_prompt = argv[1]
+args = argv[1:]
+
+def has_flag(flag):
+    if flag in argv:
+        return True
+    
+    return False
+        
+is_verbose = has_flag("--verbose")
+if is_verbose:
+    args.remove("--verbose")
+
+user_prompt = " ".join(args)
 
 messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
@@ -20,6 +32,13 @@ messages = [
 
 client = genai.Client(api_key=api_key)
 contentResponse = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
+if is_verbose:
+    verbose_messages = [
+        f"Working on: {user_prompt}",
+        f"Prompt tokens: {contentResponse.usage_metadata.prompt_token_count}",
+        f"Response tokens: {contentResponse.usage_metadata.candidates_token_count}"
+    ]
+    for log in verbose_messages:
+        print(log)
+print("Response:")
 print(contentResponse.text)
-print(f"Prompt tokens: {contentResponse.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {contentResponse.usage_metadata.candidates_token_count}")
