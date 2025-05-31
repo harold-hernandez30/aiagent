6838d920f2f25cbc762a82b717324302f90d0f7a
Harold Hernandez <harold.hernandez@visma.com>
2025-05-31 18:05:09 +0200

    Change contents to array of messages with role

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 from sys import argv, exit
 
 load_dotenv()
@@ -13,8 +14,12 @@ if len(argv) == 1:
 
 user_prompt = argv[1]
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
+
 client = genai.Client(api_key=api_key)
-contentResponse = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
+contentResponse = client.models.generate_content(model="gemini-2.0-flash-001", contents=messages)
 print(contentResponse.text)
 print(f"Prompt tokens: {contentResponse.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {contentResponse.usage_metadata.candidates_token_count}")
