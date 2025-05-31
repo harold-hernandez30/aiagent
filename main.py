1a8779d1b02cb440533cfdd2777a7c674dc7b793
Harold Hernandez <harold.hernandez@visma.com>
2025-05-31 17:49:21 +0200

    Get prompt from cli param

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from sys import argv, exit
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+
+if len(argv) == 1:
+    print("error: please provide prompt")
+    exit(1)
+
+user_prompt = argv[1]
+
 client = genai.Client(api_key=api_key)
-contentResponse = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+contentResponse = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_prompt)
 print(contentResponse.text)
 print(f"Prompt tokens: {contentResponse.usage_metadata.prompt_token_count}")
 print(f"Response tokens: {contentResponse.usage_metadata.candidates_token_count}")
